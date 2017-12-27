f21211cdc43f824f3fc73db632a6242ea6228717
Arthur Zhuk <arthurzhuk@gmail.com>
2017-12-27 13:12:26 -0800

    search

--- src/components/MyKitties.vue
+++ src/components/MyKitties.vue
@@ -1,21 +1,37 @@
 <template>
   <!-- TODO: Link this api call with the kitten one w\ the attributes -->
   <v-container>
+    <v-layout row>
+      <v-flex xs4>
+        <v-subheader>Ethereum Address</v-subheader>
+      </v-flex>
+      <v-flex xs8>
+        <v-text-field
+          name="eth-address"
+          label="Enter Your Ethereum Wallet Address to see your CryptoKitties"
+          id="eth-address"
+          single-line
+          v-model="ETH_ADDRESS"
+        ></v-text-field>
+      </v-flex>
+    </v-layout>
     <v-flex v-if="myKitties && myKitties.kitties && myKitties.kitties.length > 0" xs12>
       <v-card :key="kitty.id" v-for="kitty in myKitties.kitties" color="cyan darken-2" class="white--text">
         <v-container fluid grid-list-lg>
           <v-layout row>
             <v-flex xs7>
               <div>
-                <div :style="{ color: kitty.color }" class="headline">{{ kitty.name || kitty.id }}</div>
+                <div v-if="kitty & kitty.name" class="headline">{{ kitty.name }} - {{ kitty.id }}</div>
+                <div v-else class="headline">{{ kitty.id }}</div>
                 <div>Gen: {{ kitty.generation }}</div>
-                <div class="" v-if="kitty && kitty.auction && kitty.auction.status === 'open'"><h3>Current Auction:</h3>
+                <div v-if="kitty && kitty.auction && kitty.auction.status === 'open'"><h3>Current Auction:</h3>
                   <div>Auction Id: {{ kitty.auction.id }}</div>
                   <div>Start Price ETH: {{ kitty.auction.start_price | eth }}</div>
                   <div>End Price ETH: {{ kitty.auction.end_price | eth }}</div>
                   <div>Current Price ETH: {{ kitty.auction.end_price | eth }}</div>
                   <div>Seller Address: {{ kitty.auction.seller.address }}</div>
                 </div>
+                <div v-else>No Auction in Progress</div>
               </div>
             </v-flex>
             <v-flex xs5>
@@ -49,9 +65,43 @@ export default {
   name: 'MyKitties',
   filters: {
     eth(val) {
-      console.log('val', val);
       if (!val) return '';
       return val * 0.000000000000000001;
+    },
+  },
+  watch: {
+    ETH_ADDRESS(val) {
+      console.log('e and ethadd', val, this.ETH_ADDRESS);
+      const LIMIT = 100;
+      const OFFSET = 0;
+      axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${this.ETH_ADDRESS}&limit=${LIMIT}&offset=${OFFSET}`)
+      .then((data) => {
+        this.myKittiesMap.set('myKitties', data.data);
+        const arr = [];
+        data.data.kitties.forEach((kitty) => {
+          axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+            .then((attributeData) => {
+              arr.push(attributeData.data);
+            });
+        });
+        this.myKittiesMap.set('attributeData', arr);
+        this.myKitties = this.myKittiesMap.get('myKitties');
+        this.myKittyAttr = this.myKittiesMap.get('attributeData');
+        console.log('myKitties.kitties', this.myKitties.kitties);
+      })
+      .then(() => {
+        axios.get('https://api.cryptokitties.co/cattributes?total=true').then((data) => {
+          this.cattributes = data.data;
+        //   this.myKittyAttr.forEach((cat) => {
+        //     console.log('cat', cat);
+        //     const result = _.merge(cat.cattributes, _.map(this.cattributes, obj => (
+        //       _.pick(obj, 'description', 'total')
+        //     )));
+        //     console.log('result', result);
+        //   });
+        //   console.log('result arr', this.myKittyAttr);
+        });
+      });
     },
   },
   computed: {
@@ -67,6 +117,7 @@ export default {
   },
   data() {
     return {
+      ETH_ADDRESS: '',
       myKitties: [],
       myKittyAttr: [],
       myKittiesMap: new Map(),
@@ -82,6 +133,10 @@ export default {
       //   { text: 'Total', value: 'total' },
       // ],
     };
+  },
+  methods: {
+    searchETHAddress(e) {
+    },
   },
   mounted() {
     const ETH_PUBLIC_TOKEN = '0x6265aafc8d25b36f97181c44d0eb6693f00eba17';
