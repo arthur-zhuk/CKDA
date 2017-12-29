cccea2f312fc74c7d210ac1f47b07ed651fa362f
Arthur Zhuk <arthurzhuk@gmail.com>
2017-12-29 10:40:38 -0800

    UI update

--- src/components/MyKitties.vue
+++ src/components/MyKitties.vue
@@ -1,5 +1,4 @@
 <template>
-  <!-- TODO: Link this api call with the kitten one w\ the attributes -->
   <v-container>
     <v-layout row>
       <v-flex xs4>
@@ -15,45 +14,62 @@
         ></v-text-field>
       </v-flex>
     </v-layout>
-    <v-flex v-if="myKitties && myKitties.kitties && myKitties.kitties.length > 0" xs12>
-      <v-card :key="kitty.id" v-for="kitty in myKitties.kitties" color="cyan darken-2" class="white--text">
-        <v-container fluid grid-list-lg>
-          <v-layout row>
-            <v-flex xs7>
-              <div>
-                <div v-if="kitty & kitty.name" class="headline">{{ kitty.name }} - {{ kitty.id }}</div>
-                <div v-else class="headline">{{ kitty.id }}</div>
-                <div>Gen: {{ kitty.generation }}</div>
-                <div v-if="kitty && kitty.auction && kitty.auction.status === 'open'"><h3>Current Auction:</h3>
-                  <div>Auction Id: {{ kitty.auction.id }}</div>
-                  <div>Start Price ETH: {{ kitty.auction.start_price | eth }}</div>
-                  <div>End Price ETH: {{ kitty.auction.end_price | eth }}</div>
-                  <div>Current Price ETH: {{ kitty.auction.end_price | eth }}</div>
-                  <div>Seller Address: {{ kitty.auction.seller.address }}</div>
-                </div>
-                <div v-else>No Auction in Progress</div>
-              </div>
-            </v-flex>
-            <v-flex xs5>
+      <v-container fluid grid-list-xl>
+        <v-layout row wrap>
+          <v-flex v-if="gArr && gArr.length > 0" :key="kitty.id" v-for="kitty in gArr" xs12 sm6 md4>
+            <v-card>
               <v-card-media
                 :src="kitty.image_url"
-                height="125px"
-                contain
+                height="400px"
               ></v-card-media>
-            </v-flex>
-            <v-flex xs8>
-              <span v-if="att.id === kitty.id" style="color: white" :key="att.id" v-for="att in myKittyAttr">
-                <div 
-                  style="color: white"
-                  :key="i" v-for="(item, i) in att.cattributes">
-                  Type: {{ item.type }} Description: <span>{{ item.description }}</span>
-                </div>
-              </span>
+              <v-card-title primary-title>
+                <v-container>
+                  <v-layout row wrap>
+                    <div v-if="kitty && kitty.name" class="headline">{{ kitty.name }} - {{ kitty.id }}</div>
+                    <div v-else class="headline">{{ kitty.id }}</div>
+                  </v-layout>
+                  <v-layout row>
+                    <div class="grey--text">Generation: {{ kitty.generation }}</div>
+                  </v-layout>
+                </v-container>
+                <v-container>
+                  <v-layout v-if="kitty && kitty.cattributes.length > 0" row wrap>
+                    <v-flex>
+                      <div :key="j" v-for="(cattribute, j) in kitty.cattributes">
+                        {{ cattribute.type }}: {{ cattribute.description }}
+                      </div>
+                    </v-flex>
+                    <v-flex>
+                      <div v-if="kitty && kitty.auction && kitty.auction.status === 'open'"><h3>Current Auction:</h3>
+                        <div>Auction Id: {{ kitty.auction.id }}</div>
+                        <div>Start Price ETH: {{ kitty.auction.start_price | eth }}</div>
+                        <div>End Price ETH: {{ kitty.auction.end_price | eth }}</div>
+                        <div>Current Price ETH: {{ kitty.auction.end_price | eth }}</div>
+                        <div>Seller Address: </div>
+                        <span>{{ kitty.auction.seller.address }}</span>
+                      </div>
+                      <div v-else>No Auction in Progress</div>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-title>
+              <v-card-actions>
+                <v-btn :href="'https://www.cryptokitties.co/kitty/' + kitty.id" target="_blank" flat>Official</v-btn>
+                <v-btn :href="'https://cryptokittydex.com/kitties/' + kitty.id" target="_blank" flat color="purple">CryptoKittyDex</v-btn>
+                <v-spacer></v-spacer>
+                <v-btn icon @click.native="show = !show">
+                  <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                </v-btn>
+              </v-card-actions>
+              <v-slide-y-transition>
+                <v-card-text v-show="show">
+                  {{ kitty.bio }}
+                </v-card-text>
+              </v-slide-y-transition>
+              </v-card>
             </v-flex>
           </v-layout>
-        </v-container>
-      </v-card>
-    </v-flex>
+      </v-container>
   </v-container>
 </template>
 
@@ -71,23 +87,144 @@ export default {
   },
   watch: {
     ETH_ADDRESS(val) {
-      console.log('e and ethadd', val, this.ETH_ADDRESS);
-      const LIMIT = 100;
-      const OFFSET = 0;
-      axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${this.ETH_ADDRESS}&limit=${LIMIT}&offset=${OFFSET}`)
+      this.gArr = [];
+      // const ETH_PUBLIC_TOKEN = '0x6265aafc8d25b36f97181c44d0eb6693f00eba17';
+      const LIMIT = 1000;
+      // const OFFSET = 20;
+      axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${val}&limit=${LIMIT}&offset=0`)
+        .then((data) => {
+          this.gKArr.push(data.data);
+          this.myKittiesMap.set('myKitties', this.gKArr);
+          // const arr = [];
+          data.data.kitties.forEach((kitty) => {
+            axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+              .then((attributeData) => {
+                this.gArr.push(attributeData.data);
+              });
+          });
+          this.myKittiesMap.set('attributeData', this.gArr);
+          // this.myKitties = this.myKittiesMap.get('myKitties');
+          this.myKittyAttr = this.myKittiesMap.get('attributeData');
+        })
+        .then(() => {
+          axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${val}&limit=${LIMIT}&offset=20`)
+            .then((data) => {
+              this.gKArr.push(data.data);
+              this.myKittiesMap.set('myKitties', this.gKArr);
+              // const arr = [];
+              data.data.kitties.forEach((kitty) => {
+                axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+                  .then((attributeData) => {
+                    this.gArr.push(attributeData.data);
+                  });
+              });
+              this.myKittiesMap.set('attributeData', this.gArr);
+              // this.myKitties = this.myKittiesMap.get('myKitties');
+              this.myKittyAttr = this.myKittiesMap.get('attributeData');
+            });
+        })
+        .then(() => {
+          axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${val}&limit=${LIMIT}&offset=40`)
+            .then((data) => {
+              this.gKArr.push(data.data);
+              this.myKittiesMap.set('myKitties', this.gKArr);
+              // const arr = [];
+              data.data.kitties.forEach((kitty) => {
+                axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+                  .then((attributeData) => {
+                    this.gArr.push(attributeData.data);
+                  });
+              });
+              this.myKittiesMap.set('attributeData', this.gArr);
+              this.myKitties = this.myKittiesMap.get('myKitties');
+              this.myKittyAttr = this.myKittiesMap.get('attributeData');
+              console.log('garr', this.gArr, this.gKArr);
+            });
+        })
+        .then(() => {
+          axios.get('https://api.cryptokitties.co/cattributes?total=true').then((data) => {
+            this.cattributes = data.data;
+          //   this.myKittyAttr.forEach((cat) => {
+          //     console.log('cat', cat);
+          //     const result = _.merge(cat.cattributes, _.map(this.cattributes, obj => (
+          //       _.pick(obj, 'description', 'total')
+          //     )));
+          //     console.log('result', result);
+          //   });
+          //   console.log('result arr', this.myKittyAttr);
+          });
+        });
+    },
+  },
+  computed: {
+  },
+  data() {
+    return {
+      gArr: [],
+      gKArr: [],
+      show: false,
+      ETH_ADDRESS: '',
+      myKitties: [],
+      myKittyAttr: [],
+      myKittiesMap: new Map(),
+      cattributes: [],
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    const ETH_PUBLIC_TOKEN = '0x6265aafc8d25b36f97181c44d0eb6693f00eba17';
+    const LIMIT = 1000;
+    // const OFFSET = 20;
+    axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${ETH_PUBLIC_TOKEN}&limit=${LIMIT}&offset=0`)
       .then((data) => {
-        this.myKittiesMap.set('myKitties', data.data);
-        const arr = [];
+        this.gKArr.push(data.data);
+        this.myKittiesMap.set('myKitties', this.gKArr);
+        // const arr = [];
         data.data.kitties.forEach((kitty) => {
           axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
             .then((attributeData) => {
-              arr.push(attributeData.data);
+              this.gArr.push(attributeData.data);
             });
         });
-        this.myKittiesMap.set('attributeData', arr);
-        this.myKitties = this.myKittiesMap.get('myKitties');
+        this.myKittiesMap.set('attributeData', this.gArr);
+        // this.myKitties = this.myKittiesMap.get('myKitties');
         this.myKittyAttr = this.myKittiesMap.get('attributeData');
-        console.log('myKitties.kitties', this.myKitties.kitties);
+      })
+      .then(() => {
+        axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${ETH_PUBLIC_TOKEN}&limit=${LIMIT}&offset=20`)
+          .then((data) => {
+            this.gKArr.push(data.data);
+            this.myKittiesMap.set('myKitties', this.gKArr);
+            // const arr = [];
+            data.data.kitties.forEach((kitty) => {
+              axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+                .then((attributeData) => {
+                  this.gArr.push(attributeData.data);
+                });
+            });
+            this.myKittiesMap.set('attributeData', this.gArr);
+            // this.myKitties = this.myKittiesMap.get('myKitties');
+            this.myKittyAttr = this.myKittiesMap.get('attributeData');
+          });
+      })
+      .then(() => {
+        axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${ETH_PUBLIC_TOKEN}&limit=${LIMIT}&offset=40`)
+          .then((data) => {
+            this.gKArr.push(data.data);
+            this.myKittiesMap.set('myKitties', this.gKArr);
+            // const arr = [];
+            data.data.kitties.forEach((kitty) => {
+              axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
+                .then((attributeData) => {
+                  this.gArr.push(attributeData.data);
+                });
+            });
+            this.myKittiesMap.set('attributeData', this.gArr);
+            this.myKitties = this.myKittiesMap.get('myKitties');
+            this.myKittyAttr = this.myKittiesMap.get('attributeData');
+            console.log('garr', this.gArr, this.gKArr);
+          });
       })
       .then(() => {
         axios.get('https://api.cryptokitties.co/cattributes?total=true').then((data) => {
@@ -102,75 +239,6 @@ export default {
         //   console.log('result arr', this.myKittyAttr);
         });
       });
-    },
-  },
-  computed: {
-    // rareComputedColor(description) {
-    //   console.log('desc', description);
-    //   return { rare: 'light-green lighten-1' ? this.cattributes[description] < 5000 : '' }
-    // },
-    computedColor() {
-      // light-green lighten-1': props.item.total < 5000,
-      // 'yellow darken-1': props.item.total >= 5000 && props.item.total <= 13000,
-      // 'red darken-1': props.item.total > 13000,
-    },
-  },
-  data() {
-    return {
-      ETH_ADDRESS: '',
-      myKitties: [],
-      myKittyAttr: [],
-      myKittiesMap: new Map(),
-      cattributes: [],
-      // headers: [
-      //   {
-      //     text: 'My Kitties',
-      //     align: 'left',
-      //     sortable: false,
-      //     value: 'myKitties',
-      //   },
-      //   { text: 'Type', value: 'type' },
-      //   { text: 'Total', value: 'total' },
-      // ],
-    };
-  },
-  methods: {
-    searchETHAddress(e) {
-    },
-  },
-  mounted() {
-    const ETH_PUBLIC_TOKEN = '0x6265aafc8d25b36f97181c44d0eb6693f00eba17';
-    const LIMIT = 100;
-    const OFFSET = 0;
-
-    axios.get(`https://api.cryptokitties.co/kitties?owner_wallet_address=${ETH_PUBLIC_TOKEN}&limit=${LIMIT}&offset=${OFFSET}`)
-    .then((data) => {
-      this.myKittiesMap.set('myKitties', data.data);
-      const arr = [];
-      data.data.kitties.forEach((kitty) => {
-        axios.get(`https://api.cryptokitties.co/kitties/${kitty.id}`)
-          .then((attributeData) => {
-            arr.push(attributeData.data);
-          });
-      });
-      this.myKittiesMap.set('attributeData', arr);
-      this.myKitties = this.myKittiesMap.get('myKitties');
-      this.myKittyAttr = this.myKittiesMap.get('attributeData');
-      console.log('myKitties.kitties', this.myKitties.kitties);
-    })
-    .then(() => {
-      axios.get('https://api.cryptokitties.co/cattributes?total=true').then((data) => {
-        this.cattributes = data.data;
-      //   this.myKittyAttr.forEach((cat) => {
-      //     console.log('cat', cat);
-      //     const result = _.merge(cat.cattributes, _.map(this.cattributes, obj => (
-      //       _.pick(obj, 'description', 'total')
-      //     )));
-      //     console.log('result', result);
-      //   });
-      //   console.log('result arr', this.myKittyAttr);
-      });
-    });
   },
 };
 </script>
